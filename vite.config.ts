53f56e686a946ff7630f2309473243d47a114404
Yücel Gümüş <yucelgumus61@yahoo.com>
2025-12-24 22:41:52 +0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/GeoGemini/',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
